c5c50b423739bca17c87512bb089ceaca39298e6
Dimitre <dimitre@users.noreply.github.com>
2024-11-08 13:07:18 -0300

    v++

--- ofbuild.rb
+++ ofbuild.rb
@@ -1,8 +1,8 @@
 class Ofbuild < Formula
   desc "Simple command-line tool for sending OSC packet"
   homepage "https://github.com/dimitre/ofbuild"
-  url "https://github.com/dimitre/ofbuild/archive/refs/tags/v0.0.9.tar.gz"
-  sha256 "363c76c751305ea092601c3c8665a0432ed886fa9543ba0a4113bd45e121a776"
+  url "https://github.com/dimitre/ofbuild/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "76317b76a0941ef14ee6114acfa1997b3f4daf9344827c0cff6449cde3f52875"
   depends_on "yaml-cpp"
   depends_on "fmt"
 
